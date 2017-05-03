fa7e00d1e0fc46a4bac34d98a04be2f108df7e60
wimjjj <wim.jongeneel1@gmail.com>
2017-05-03 22:29:57 +0200

    updated AddTask form

--- Client/Components/AddTask.js
+++ Client/Components/AddTask.js
@@ -7,10 +7,12 @@ export default class Addtask extends React.Component{
         let title = titleInput.value;
         titleInput.value= "";
 
+        let labelID = document.getElementById("label").value;
+
         let task = {
             title,
             done: false,
-            LabelID: 1
+            labelID,
         };
 
         this.props.addTask(task);
@@ -20,8 +22,15 @@ export default class Addtask extends React.Component{
         return (
             <div class="row">
                 <form onSubmit={function(e){e.preventDefault()}}>
-                    <div class="ten columns">
+                    <div class="eight columns">
                         <input type="text" name="title" id="title" placeholder="new task" class="u-full-width" autoFocus/>
+                    </div>
+                    <div class="two columns">
+                        <select id="label" class="u-full-width">
+                            <option value="1">School</option>
+                            <option value="2">Work</option>
+                            <option value="3">Private</option>
+                        </select>
                     </div>
                     <div class="two columns">
                         <button onClick={this.addTask.bind(this)} class="button button-primary u-full-width">add</button>
